2a6990bc0c16be8d395d7fd75ee0447de05dcd6d
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2026-08-07 13:09:25 +0530

    Update metadataBase and add og:url property

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,13 +11,14 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   metadataBase: new URL(
     process.env.NEXT_PUBLIC_SITE_URL ||
-    (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000")
+    (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "https://www.persuaide.ai")
   ),
   title: "Persuaide",
   description: "Persuasive campaigns in seconds",
   openGraph: {
     title: "Persuaide",
     description: "Persuasive campaigns in seconds",
+    url: "/",
     type: "website",
     images: [
       {
